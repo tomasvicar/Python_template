1abcf751b5093ebec484dd9c3513834562066f99
tomasvicar <tomasvicar@gmail.com>
2023-02-16 13:17:52 +0100

    remove python version - je to blbost

--- create_env_windows.bat
+++ create_env_windows.bat
@@ -1,4 +1,4 @@
-python -m venv ../env python=3.10
+python -m venv ../env
 CALL "..\env\Scripts\activate.bat"
 pip install -r requirements.txt
 start cmd /k CALL "..\env\Scripts\activate.bat" /k echo env is ready...
